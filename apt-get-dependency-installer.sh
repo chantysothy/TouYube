6cde55af8bfc39e1d1ef001655f409b4dea3b72c
Donovan <tronovan@gmail.com>
2016-07-29 21:22:20 -0500

    Update apt-get-dependency-installer.sh

--- apt-get-dependency-installer.sh
+++ apt-get-dependency-installer.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 sudo apt-get update
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install -y nodejs youtube-dl 
 
+#this downloads nodejs
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs 
+
+#this installs youtube-dl
+sudo apt-get install youtube-dl 
+#this makes sure it's the latest.
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
